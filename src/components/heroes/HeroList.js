220580c6d80152923ca8ac68e3a22347a477ebea
Shineily <shineily@hotmail.com>
2020-09-18 11:54:22 -0500

    others

---
+++ src/components/heroes/HeroList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { getHeroesByPublisher } from '../../selectors/getHeriesByPublisher';
+import { HeroCard } from './HeroCard';
+
+export const HeroList = ({ publisher }) => {
+  const heroes = getHeroesByPublisher(publisher);
+
+  return (
+    <div className='card-columns animate__animated animate__fadeIn'>
+      {heroes.map((hero) => (
+        <HeroCard key={hero.id} {...hero} />
+      ))}
+    </div>
+  );
+};
